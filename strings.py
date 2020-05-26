e83705ef10b64ae419518e09b8da8c3d18f7202c
AhmedAY <ahmedal-yaqoobi@ese-octomac3.home>
2020-05-26 09:23:26 +0400

    added numbers code

---
+++ strings.py
@@ -0,0 +1,13 @@
+name = 'Ahmed'
+print(name)
+id(name)
+print(id(name))
+hex(id(name))
+print(hex(id(name)))
+fname = 'Ahmed'
+lname = 'Al Yaqoobi'
+print(fname.upper() + " " + lname)
+full_name = 'Ahmed Al Yaqoogi'
+print(full_name.replace('Ahmed','Ali'))
+b = 'A' * 500
+print(b)
