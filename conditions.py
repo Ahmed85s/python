a8707f0e31122eb4d1022c882ef3a7f459261fa1
AhmedAY <ahmedal-yaqoobi@ese-octomac3.home>
2020-05-26 11:17:57 +0400

    conditions

---
+++ conditions.py
@@ -0,0 +1,6 @@
+username = input('please insert your username: \n')
+password = input('please insert your password: \n')
+if username == 'Ahmed' and password == '123': 
+    print('Welcome to your email')
+else:
+    print('Error')
