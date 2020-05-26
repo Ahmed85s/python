6ac12b2360d33328385b283960cf5648500f1f4e
AhmedAY <ahmedal-yaqoobi@ese-octomac3.home>
2020-05-26 11:59:53 +0400

    functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+def liner():
+    print('-----------------------')
+
+
+print('Ahmed Musallam')
+liner()
+print(34)
+liner()
+
+
+def addition(num1,num2):
+    print(int(num1) + int(num2))
+
+n1 = input('please enter the first number: \n')
+n2 = input('please enter the second number: \n')
+addition(n1,n2)
+
+
